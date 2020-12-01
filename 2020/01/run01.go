ef2b0f9cbd128036936b163cc59e5103343a8b50
Mx <git@mx.ouda.fr>
2020-12-01 22:25:59 +0100

    Ajout de fix_expense

--- 2020/01/run01.go
+++ 2020/01/run01.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-  "fmt"
   "errors"
+  "fmt"
+  "os"
 )
 
 // find_pair returns the two firsts elements whose sum equal 2020
@@ -19,7 +20,16 @@ func find_pair(s []int) (int, int, error) {
   return 0, 0, errors.New("can't find pair whose sum give 2020")
 }
 
-func main() {
+// 
+func fix_expense (s []int) (int, error) {
+  number_a, number_b, err := find_pair(s)
+  if err != nil {
+    return 0, err
+  }
+  return number_a * number_b, nil
+}
+
+func example_one() {
   input_numbers := make([]int, 6)
   input_numbers[0] = 1721
   input_numbers[1] = 979
@@ -28,6 +38,16 @@ func main() {
   input_numbers[4] = 675
   input_numbers[5] = 1456
   fmt.Printf("Input: %d\n", input_numbers)
-  number_a, number_b, _ := find_pair(input_numbers)
-  fmt.Printf("%d and %d\n", number_a, number_b)
+  result, _ := fix_expense(input_numbers)
+  fmt.Printf("Output: %d\n", result)
+}
+
+func main() {
+  request := "all"
+  if len(os.Args) > 1 {
+    request = os.Args[1]
+  }
+  if request == "1" || request == "example1" || request == "all" {
+    example_one()
+  }
 }
--- 2020/01/run01_test.go
+++ 2020/01/run01_test.go
@@ -12,12 +12,32 @@ func TestFindPair(t *testing.T) {
   input_numbers[5] = 1456
   pair_result_a, pair_result_b, err := find_pair(input_numbers)
   if err != nil {
-    t.Error("FindPair: Do not find any valid pair on example test")
+    t.Error("find_pair: Do not find any valid pair on example test")
   } else if (pair_result_a == 1721 && pair_result_b == 299) {
-    t.Log("FindPair: Sucess on example test")
+    t.Log("find_pair: Sucess on example test")
   } else if (pair_result_b == 1721 && pair_result_a == 299) {
-    t.Log("FindPair: Sucess on example test")
+    t.Log("find_pair: Sucess on example test")
   } else {
-    t.Errorf("FindPair: Do not return 1721 and 299 on example test but %v and %v", pair_result_a, pair_result_b)
+    t.Errorf("find_pair: Do not return 1721 and 299 on example test but %v and %v", pair_result_a, pair_result_b)
+  }
+}
+
+func TestFixExpense(t *testing.T) {
+  input_numbers := make([]int, 6)
+  input_numbers[0] = 1721
+  input_numbers[1] = 979
+  input_numbers[2] = 366
+  input_numbers[3] = 299
+  input_numbers[4] = 675
+  input_numbers[5] = 1456
+  given_multiple, err := fix_expense(input_numbers)
+  expected_multiple := 514579
+  if err != nil {
+    t.Error("fix_expense: Do not find any valid pair on example test")
+  } else if (given_multiple == expected_multiple) {
+    t.Log("fix_expense: Sucess on example test")
+  } else {
+    t.Errorf("fix_expense: Do not return %v on example test but %v",
+             expected_multiple, given_multiple)
   }
 }
