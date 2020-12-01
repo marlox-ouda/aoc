a6fb121ef4f40fba3ab6d7efc19764fba3bd7a73
Mx <git@mx.ouda.fr>
2020-12-01 23:10:35 +0100

    Ajout de readInput

--- 2020/01/run01.go
+++ 2020/01/run01.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+  "bufio"
   "errors"
   "fmt"
   "os"
+  "strconv"
 )
 
 // find_pair returns the two firsts elements whose sum equal 2020
 // take int slice as input
 // third returned value is error or nil
-func find_pair(s []int) (int, int, error) {
+func findPair(s []int) (int, int, error) {
   for index, number_a := range s {
     for _, number_b := range s[index:] {
       if number_a + number_b == 2020 {
@@ -21,15 +23,36 @@ func find_pair(s []int) (int, int, error) {
 }
 
 // 
-func fix_expense (s []int) (int, error) {
-  number_a, number_b, err := find_pair(s)
+func fixExpense (s []int) (int, error) {
+  number_a, number_b, err := findPair(s)
   if err != nil {
     return 0, err
   }
   return number_a * number_b, nil
 }
 
-func example_one() {
+func readInput( ) ([]int, error) {
+  input_fd, err := os.Open("./input1.txt")
+  if err != nil {
+    return nil, err
+  }
+  defer input_fd.Close()
+  scanner := bufio.NewScanner(input_fd)
+  var numbers_slice []int
+  var casted_number int
+  var line string
+  for scanner.Scan() {
+    line = scanner.Text()
+    casted_number, err = strconv.Atoi(line)
+    if err != nil {
+      return nil, fmt.Errorf("can't cast %v to integer", line)
+    }
+    numbers_slice = append(numbers_slice, casted_number)
+  }
+  return numbers_slice, nil
+}
+
+func exampleOne() {
   input_numbers := make([]int, 6)
   input_numbers[0] = 1721
   input_numbers[1] = 979
@@ -38,8 +61,12 @@ func example_one() {
   input_numbers[4] = 675
   input_numbers[5] = 1456
   fmt.Printf("Input: %d\n", input_numbers)
-  result, _ := fix_expense(input_numbers)
+  result, _ := fixExpense(input_numbers)
   fmt.Printf("Output: %d\n", result)
+}
+
+func one() {
+  readInput()
 }
 
 func main() {
@@ -48,6 +75,9 @@ func main() {
     request = os.Args[1]
   }
   if request == "1" || request == "example1" || request == "all" {
-    example_one()
+    exampleOne()
+  }
+  if request == "1" || request == "input1" || request == "all" {
+    one()
   }
 }
--- 2020/01/run01_test.go
+++ 2020/01/run01_test.go
@@ -10,15 +10,15 @@ func TestFindPair(t *testing.T) {
   input_numbers[3] = 299
   input_numbers[4] = 675
   input_numbers[5] = 1456
-  pair_result_a, pair_result_b, err := find_pair(input_numbers)
+  pair_result_a, pair_result_b, err := findPair(input_numbers)
   if err != nil {
-    t.Error("find_pair: Do not find any valid pair on example test")
+    t.Error("findPair: Do not find any valid pair on example test")
   } else if (pair_result_a == 1721 && pair_result_b == 299) {
-    t.Log("find_pair: Sucess on example test")
+    t.Log("findPair: Sucess on example test")
   } else if (pair_result_b == 1721 && pair_result_a == 299) {
-    t.Log("find_pair: Sucess on example test")
+    t.Log("findPair: Sucess on example test")
   } else {
-    t.Errorf("find_pair: Do not return 1721 and 299 on example test but %v and %v", pair_result_a, pair_result_b)
+    t.Errorf("findPair: Do not return 1721 and 299 on example test but %v and %v", pair_result_a, pair_result_b)
   }
 }
 
@@ -30,14 +30,37 @@ func TestFixExpense(t *testing.T) {
   input_numbers[3] = 299
   input_numbers[4] = 675
   input_numbers[5] = 1456
-  given_multiple, err := fix_expense(input_numbers)
+  given_multiple, err := fixExpense(input_numbers)
   expected_multiple := 514579
   if err != nil {
-    t.Error("fix_expense: Do not find any valid pair on example test")
+    t.Error("fixExpense: Do not find any valid pair on example test")
   } else if (given_multiple == expected_multiple) {
-    t.Log("fix_expense: Sucess on example test")
+    t.Log("fixExpense: Sucess on example test")
   } else {
-    t.Errorf("fix_expense: Do not return %v on example test but %v",
+    t.Errorf("fixExpense: Do not return %v on example test but %v",
              expected_multiple, given_multiple)
+  }
+}
+
+func TestReadInput(t *testing.T) {
+  input_numbers, err := readInput()
+  if err != nil {
+    t.Errorf("readInput: fail with %s error", err)
+  }
+  expected_first_number := 1918
+  expected_last_number := 1407
+  expected_len := 200
+  if input_numbers[0] != expected_first_number {
+    t.Errorf("readInput: Not expected first number, %v expected, %v given",
+             expected_first_number, input_numbers[0])
+  }
+  given_len := len(input_numbers)
+  if given_len != expected_len {
+    t.Errorf("readInput: Not expected slice len, %v expected, %v given",
+             expected_len, given_len)
+  }
+  if input_numbers[given_len-1] != expected_last_number {
+    t.Errorf("readInput: Not expected last number, %v expected, %v given",
+             expected_last_number, input_numbers[given_len-1])
   }
 }
