f189b3db5140f324deb20633df719228d1e1ef44
Mx <git@mx.ouda.fr>
2020-12-04 23:14:07 +0100

    Duplicate part1 for part2 and clean run03 old code

--- 2020/04/run04.go
+++ 2020/04/run04.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-  "bufio"
   "io/ioutil"
   "fmt"
   "os"
@@ -96,126 +95,6 @@ func checkPassportRequiredField(pass *passport) bool {
   return false
 }
 
-var example_geology = []string{
-  "..##.......",
-  "#...#...#..",
-  ".#....#..#.",
-  "..#.#...#.#",
-  ".#...##..#.",
-  "..#.##.....",
-  ".#.#.#....#",
-  ".#........#",
-  "#.##...#...",
-  "#...##....#",
-  ".#..#...#.#",
-}
-
-// isTree return whether there is a three or no at the given position
-// position is given 0 based and take top left corner as (0, 0) position
-func isTree(grid []string, vertical_offset int, horizontal_offset int) bool {
-  line_len := len(grid[0])
-  if vertical_offset >= len(grid) {
-    return false
-  }
-  horizontal_offset = horizontal_offset % line_len
-  if grid[vertical_offset][horizontal_offset] == '#' {
-    return true
-  }
-  return false
-}
-
-// count encounter trees from the top to the bottom of the grid
-func countTreesOnDirection(grid []string, horizontal_step int, vertical_step int) int {
-  var (
-    current_vertical_offset int
-    current_horizontal_offset int
-    currenly_encountered_trees int
-  )
-  if isTree(grid, current_vertical_offset, current_horizontal_offset) {
-      currenly_encountered_trees += 1
-  }
-  for current_vertical_offset < len(grid) {
-    current_vertical_offset += vertical_step
-    current_horizontal_offset += horizontal_step
-    if isTree(grid, current_vertical_offset, current_horizontal_offset) {
-      currenly_encountered_trees += 1
-    }
-  }
-  return currenly_encountered_trees
-}
-
-// multiply every component of the slice between them and return the value
-func multiply(values []int) int {
-  var (
-    total int
-    current_value int
-  )
-  total = 1
-  for _, current_value = range values {
-    total *= current_value
-  }
-  return total
-}
-
-
-// checkPolicyOne if letter occurence in password is between min and max
-func checkPolicyOne(min int, max int, letter byte, password string) bool {
-  var count int
-  count = strings.Count(password, string(letter))
-  if min <= count && count <= max {
-    return true
-  }
-  return false
-}
-
-// checkPolicyTwo if letter is present at only one given of both
-// given position in the password
-func checkPolicyTwo(pos_a int, pos_b int, letter byte, password string) bool {
-  var (
-    check_a bool
-    check_b bool
-  )
-  check_a = (password[pos_a - 1] == letter)
-  check_b = (password[pos_b - 1] == letter)
-  return check_a != check_b
-}
-
-// convertLine
-// read element from the given string line
-func convertLine(line string) (int, int, byte, string, error) {
-  var (
-    number_a int
-    number_b int
-    letter byte
-    password string
-    err error
-    parsed_items_nb int
-  )
-  parsed_items_nb, err = fmt.Fscanf(strings.NewReader(line), "%d-%d %c: %s",
-                                   &number_a, &number_b, &letter, &password)
-  if err == nil && parsed_items_nb < 4 {
-    err = fmt.Errorf("Only able to parse %v/4 items on %s", parsed_items_nb, line)
-  }
-  return number_a, number_b, letter, password, err
-}
-
-// Read input1.txt file and return a slice of int
-// considering each line is just containing one int
-func readInput( ) ([]string, error) {
-  input_fd, err := os.Open("./input3.txt")
-  if err != nil {
-    return nil, err
-  }
-  defer input_fd.Close()
-  scanner := bufio.NewScanner(input_fd)
-  var lines []string
-  var line string
-  for scanner.Scan() {
-    line = scanner.Text()
-    lines = append(lines, line)
-  }
-  return lines, nil
-}
 
 func commonOne(passports_blob string) int {
   var (
@@ -261,43 +140,47 @@ func mainOne() {
   }
 }
 
-func runTwo(grid []string) int {
+func commonTwo(passports_blob string) int {
   var (
-    trees_number int
-    trees_numbers []int
-    direction []int
-    horizontal_step int
-    vertical_step int
+    valid_passports_number int
+    passport_lines []string
+    passport_line string
+    pass *passport
+    err error
   )
-  directions := [][]int{
-    []int{1, 1},
-    []int{3, 1},
-    []int{5, 1},
-    []int{7, 1},
-    []int{1, 2},
+  passport_lines = extractPassportLines(passports_blob)
+  for _, passport_line = range passport_lines {
+    pass, err = extractPassportData(passport_line)
+    if err == nil {
+      if checkPassportRequiredField(pass) {
+        valid_passports_number += 1
+      }
+    } else {
+      fmt.Printf("Error: %s\n", err)
+    }
   }
-
-  for _, direction = range directions {
-    horizontal_step = direction[0]
-    vertical_step = direction[1]
-    fmt.Printf("Direction: (%v, %v) -> ", horizontal_step, vertical_step)
-    trees_number = countTreesOnDirection(grid, horizontal_step, vertical_step)
-    fmt.Printf("%v tree(s)\n", trees_number)
-    trees_numbers = append(trees_numbers, trees_number)
-  }
-  return multiply(trees_numbers)
- }
-
-func exampleTwo() {
-  fmt.Printf("ExampleTwo: %v\n", runTwo(example_geology))
+  return valid_passports_number
 }
 
+func exampleTwo() {
+  var valid_passports_number int
+  valid_passports_number = commonOne(example_passwords_input)
+  fmt.Printf("ExampleTwo: %v\n", valid_passports_number)
+}
+
+
 func mainTwo() {
-  lines, err := readInput()
-  if err != nil {
-    fmt.Println(err)
+  var (
+    valid_passports_number int
+    input []byte
+    err error
+  )
+  input, err = ioutil.ReadFile("input4.txt")
+  if err == nil {
+    valid_passports_number = commonOne(string(input))
+    fmt.Printf("MainTwo: %v\n", valid_passports_number)
   } else {
-    fmt.Printf("MainTwo: %v\n", runTwo(lines))
+    fmt.Printf("MainTwo: I/O erro : %s\n", err)
   }
 }
 
